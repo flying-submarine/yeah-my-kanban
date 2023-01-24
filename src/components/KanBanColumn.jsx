c281f1405935bd4a26ab373d7be925bc9b4e8b3d
submarine <532384048@qq.com>
2023-01-24 19:49:06 +0800

    初始化 vite

---
+++ src/components/KanBanColumn.jsx
@@ -0,0 +1,104 @@
+import { css } from '@emotion/react';
+import KanbanCard from './KanbanCard';
+import { useState } from 'react';
+import KanbanNewCard from './KanbanNewCard';
+
+
+export default function KanBanColumn({
+  children,
+  bgColor,
+  title,
+  setIsDragSource = () => { },
+  setIsDragTarget = () => { },
+  handleDrop,
+  cardList=[],
+  setDraggedItem,
+  canAddNew=false,
+  onAdd,
+  onRemove
+}) {
+  const [showAdd, setShowAdd] = useState(false); 
+
+  const handleAdd = () => {  
+    setShowAdd(true) 
+  };
+  const handleSubmit = (newCard)=>{
+    onAdd && onAdd(newCard)
+    setShowAdd(false)
+  }
+  return (
+    <section
+      onDragStart={() => setIsDragSource(true)}
+      onDragOver={(evt) => {
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = 'move';
+        setIsDragTarget(true);
+      } }
+      onDragLeave={(evt) => {
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = 'none';
+        setIsDragTarget(false);
+      } }
+      onDrop={(evt) => {
+        evt.preventDefault();
+        handleDrop && handleDrop(evt);
+      } }
+      onDragEnd={(evt) => {
+        evt.preventDefault();
+        setIsDragSource(false);
+        setIsDragTarget(false);
+      } }
+      css={KanBanColumnStyle(bgColor)}
+    >
+      <h2>
+        {title}
+        {canAddNew && 
+        <button onClick={handleAdd} disabled={showAdd}>
+        ⊕ 添加新卡片
+        </button>
+      }
+      </h2>
+      <ul>
+        {children}
+        {showAdd && <KanbanNewCard onSubmit={handleSubmit}/>}
+        {cardList.map(props => <KanbanCard 
+         onDragStart={()=>setDraggedItem && setDraggedItem(props)}
+        {...props}  
+        onRemove={onRemove}
+        key={props.title}/>) }
+      </ul>
+
+    </section>
+  );
+}
+function KanBanColumnStyle(bgColor) {
+  return css`
+        flex: 1 1;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid gray;
+        border-radius: 1rem;
+        background-color: ${bgColor};
+        & > ul {
+          flex: 0px;
+          margin: 1rem;
+          padding: 0;
+          overflow: auto;
+        }
+        & > h2 {
+          margin: 0.6rem 1rem;
+          padding-bottom: 0.6rem;
+          border-bottom: 1px solid gray;
+          & > button { 
+            float: right; 
+            margin-top: 0.2rem;
+            padding: 0.2rem 0.5rem; 
+            border: 0; 
+            border-radius: 1rem; 
+            height: 1.8rem; 
+            line-height: 1rem; 
+            font-size: 1rem;} 
+        }
+      `;
+}
+
---
+++ src/components/KanbanCard.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect, useContext } from 'react';
+import AdminContext from '../context/AdminContext';
+import { css } from '@emotion/react';
+import { MINUTE, HOUR, DAY, UPDATE_INTERVAL } from './App';
+
+const kanbanCardStylesTime = css`
+  text-align: right;
+  font-size: 0.8rem;
+  color: #333;`
+;
+
+const kanbanCardStylesTitle = css`
+  min-height: 3rem;
+`;
+const kanbanCardStyles = css`
+  margin-bottom: 1rem;
+  padding: 0.6rem 1rem;
+  border: 1px solid gray;
+  border-radius: 1rem;
+  list-style: none;
+  background-color: rgba(255, 255, 255, 0.4);
+  text-align: left;
+  &:hover{
+    box-shadow: 0 0.2rem 0.2rem rgba(0,0,0,0.2),inset 0 1px #fff;
+  }
+`;
+export default function KanbanCard({ title, status, onDragStart, onRemove}) {
+
+  const [displayTime, setDisplayTime] = useState(status);
+  const isAdmin = useContext(AdminContext);
+
+  useEffect(() => {
+    const updateDisplayTime = () => {
+      const timePassed = new Date() - new Date(status);
+      let relativeTime = '刚刚';
+      if (MINUTE <= timePassed && timePassed < HOUR) {
+        relativeTime = `${Math.ceil(timePassed / MINUTE)} 分钟前`;
+      } else if (HOUR <= timePassed && timePassed < DAY) {
+        relativeTime = `${Math.ceil(timePassed / HOUR)} 小时前`;
+      } else if (DAY <= timePassed) {
+        relativeTime = `${Math.ceil(timePassed / DAY)} 天前`;
+      }
+      setDisplayTime(relativeTime);
+    };
+    const intervalId = setInterval(updateDisplayTime, UPDATE_INTERVAL);
+    updateDisplayTime();
+
+    return function cleanup() {
+      clearInterval(intervalId);
+    };
+  }, [status]);
+
+  const handleDragStart = (evt) => {
+    evt.dataTransfer.effectAllowed = "move";
+    evt.dataTransfer.setData("text/plain", title);
+    onDragStart && onDragStart();
+  };
+  return (
+    <li css={kanbanCardStyles} draggable onDragStart={handleDragStart}>
+      <div css={kanbanCardStylesTitle}>{title}</div>
+      <div css={kanbanCardStylesTime} title={status}>
+        {displayTime}
+        {isAdmin && onRemove && <button onClick={() => onRemove({title})}>X</button>}
+      </div>
+    </li>
+  );
+}
